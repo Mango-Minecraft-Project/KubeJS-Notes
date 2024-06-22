f055f77a98327e3e7b0fde3a9581bd43db0528b6
Mango Jelly Pudding <74277414+EvanHsieh0415@users.noreply.github.com>
2024-06-23 01:29:48 +0800

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/KubeJS-Notes/",
+  base: "/",
   port: 9090,
   // open: true,
 
