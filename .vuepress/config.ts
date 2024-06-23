f8802d34c9c1bbbc93a9b0afe173dd7184aa2df4
Mango <evan940415@gmail.com>
2024-06-23 08:43:30 +0800

    add meta tags

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -12,4 +12,12 @@ export default defineUserConfig({
   description: "一些關於 Minecraft 的 KubeJS 筆記",
 
   theme,
+
+  head: [
+    ["meta", { property: "og:image", content: "https://raw.githubusercontent.com/KubeJS-Mods/KubeJS/2001/common/src/main/resources/kubejs_logo.png" }],
+    ["meta", { property: "og:image:type", content: "image/png" }],
+    ["meta", { property: "og:image:width", content: "128" }],
+    ["meta", { property: "og:image:height", content: "128" }],
+    ["meta", { property: "theme-color", content: "#422066" }],
+  ]
 });
